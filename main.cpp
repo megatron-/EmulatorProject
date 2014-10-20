0ba547afb4fde9fcc6ae539eed68f546ea725d55
Titan <megatron_1337@hotmail.com>
2014-10-20 12:42:14 +0100

    First main
    Compiles but untested

---
+++ main.cpp
@@ -0,0 +1,24 @@
+int main()
+{
+	bool TripleFault = false;
+	int TripleCount = 0;
+	Memory MemoryBank;
+	Processor CPU(MemoryBank);
+	while(CPU.Ready() && !TripleFault)
+	{
+		try
+		{
+			CPU.Fetch();
+			CPU.Decode();
+		}
+		catch(const char* e)
+		{
+			std::cout << "Error: " << e << "\n";
+			TripleCount++;
+			if(TripleCount >= 3) TripleFault = true;
+		}
+	}
+	if(TripleFault)
+		std::cout << "Processor experienced a triple fault - CPU halted\n";
+	return 0;
+}
